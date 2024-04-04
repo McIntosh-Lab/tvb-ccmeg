4be4f4672e5d40f2e9a45a4734f195fa2d61d7a9
Simon Dobri <sdobri@cedar5.cedar.computecanada.ca>
2024-04-04 11:25:33 -0700

    removed unused code

--- camcan/processing/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Data processing tools."""
-from .permutation_importance import permutation_importance
-from .spoc import SPoC
-from .stacking import StackingRegressor
-from .tangent import map_tangent
-
-__all__ = ['permutation_importance', 'StackingRegressor',
-           'SPoC', 'map_tangent']
