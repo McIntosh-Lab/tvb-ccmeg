5e587dc53ca7b6ae1be2eebb229559ac0b6954b9
JP-Solomon <jpsolomo@sfu.ca>
2024-08-02 14:03:05 -0700

    starting new branch and updating freesurfer batch scripts

--- freesurfer_scripts/freesurfer_run_sub-CC320022.sh
+++ freesurfer_scripts/freesurfer_run_sub-CC320022.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --job-name=freesurfer-recon-sub-CC320022
-#SBATCH --output=recon-sub-CC320022.out
-#SBATCH --error=recon-sub-CC320022.err
-#SBATCH --mem=8G
+#SBATCH --output=logs/recon_out/recon-sub-CC320022.out
+#SBATCH --error=logs/recon_err/recon-sub-CC320022.err
+#SBATCH --mem=8000M
 #SBATCH --account=rrg-rmcintos
-FREESURFER_HOME='/home/pmahon/research/INN/software/freesurfer/7.4.1'
+export FREESURFER_HOME=/home/pmahon/research/INN/software/freesurfer/7.4.1
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
-SUBJECTS_DIR='/home/sdobri/scratch/Cam-CAN/freesurfer'
+SUBJECTS_DIR='/home/jsolomon/scratch/cam-Can/freesurfer'
 recon-all -skullstrip -gcut -clean-bm -subjid sub-CC320022
 recon-all -autorecon2 -autorecon3 -subjid sub-CC320022
--- freesurfer_scripts/freesurfer_run_sub-CC721704.sh
+++ freesurfer_scripts/freesurfer_run_sub-CC721704.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --job-name=freesurfer-recon-sub-CC721704
-#SBATCH --output=recon-sub-CC721704.out
-#SBATCH --error=recon-sub-CC721704.err
-#SBATCH --mem=8G
+#SBATCH --output=./logs/recon_out/recon-sub-CC721704.out
+#SBATCH --error=./logs/recon_err/recon-sub-CC721704.err
+#SBATCH --mem=8000M
 #SBATCH --account=rrg-rmcintos
-FREESURFER_HOME='/home/pmahon/research/INN/software/freesurfer/7.4.1'
+export FREESURFER_HOME=/home/pmahon/research/INN/software/freesurfer/7.4.1
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
-SUBJECTS_DIR='/home/sdobri/scratch/Cam-CAN/freesurfer'
+SUBJECTS_DIR='/home/jsolomon/scratch/cam-Can/freesurfer'
 recon-all -skullstrip -wsthresh 15 -clean-bm -no-wsgcaatlas -subjid sub-CC721704recon-all -autorecon2 -autorecon3 -subjid sub-CC721704
