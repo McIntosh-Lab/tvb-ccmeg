42756e94573d320d6fefc4517f5c29cc8809b83b
JP-Solomon <jpsolomo@sfu.ca>
2024-11-28 16:48:20 -0800

    adding updated paths for freesurfer scripts

--- freesurfer_scripts/freesurfer_run_sub-CC721704.sh
+++ freesurfer_scripts/freesurfer_run_sub-CC721704.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --job-name=freesurfer-recon-sub-CC721704
-#SBATCH --output=recon-sub-CC721704.out
-#SBATCH --error=recon-sub-CC721704.err
+#SBATCH --output=./logs/freesurfer/output/recon-sub-CC721704.out
+#SBATCH --error=./logs/freesurfer/error/recon-sub-CC721704.err
 #SBATCH --mem=8G
 #SBATCH --account=rrg-rmcintos
-FREESURFER_HOME='/home/pmahon/research/INN/software/freesurfer/7.4.1'
+
+#This script must be run after "./freesurfer_scripts/freesurfer_script.sh".
+
+#Load FreeSurfer
+export FREESURFER_HOME=/home/pmahon/research/INN/software/freesurfer/7.4.1
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
-SUBJECTS_DIR='/home/sdobri/scratch/Cam-CAN/freesurfer'
+
+#Define output directory
+SUBJECTS_DIR="$(pwd)/_Data/freesurfer"
+
+#Run altered command for sub-CC721704
 recon-all -skullstrip -wsthresh 15 -clean-bm -no-wsgcaatlas -subjid sub-CC721704recon-all -autorecon2 -autorecon3 -subjid sub-CC721704
--- freesurfer_scripts/freesurfer_script.sh
+++ freesurfer_scripts/freesurfer_script.sh
@@ -2,18 +2,36 @@
 #SBATCH --array=0-653
 #SBATCH --time=24:00:00
 #SBATCH --job-name=freesurfer-recon
-#SBATCH --output=recon-%A_%a.out
-#SBATCH --error=recon-%A_%a.err
+#SBATCH --output=./logs/freesurfer/output/recon-%A_%a.out
+#SBATCH --error=./logs/freesurfer/error/recon-%A_%a.err
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=8G
 #SBATCH --account=rrg-rmcintos
-FREESURFER_HOME='/home/pmahon/INN/software/freesurfer/7.4.1'
+
+#Load Freesurfer
+export FREESURFER_HOME=/home/pmahon/research/INN/software/freesurfer/7.4.1
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
+
 shopt -s nullglob
-SUBJECTS_DIR='/home/sdobri/scratch/Cam-CAN/freesurfer/'
-rawdir='/home/sdobri/projects/def-rmcintos/Cam-CAN/mri/raw/'
+
+#Define and create output directory
+SUBJECTS_DIR="$(pwd)/_Data/freesurfer/"
+
+if [ ! -e "$(pwd)/_Data/freesurfer" ]; then
+  mkdir -p "$(pwd)/_Data/freesurfer"
+  echo "FreeSurfer directory created."
+else
+  echo "FreeSurfer directory already exists."
+fi
+
+#Define input directory
+rawdir="$HOME/projects/def-rmcintos/Cam-CAN/mri/raw/"
 cd $rawdir
+
+#Define current subject and start job
 subs=(sub*)
 subc=${subs[$SLURM_ARRAY_TASK_ID]}
+
 echo "Starting subject $subc"
+
 recon-all -subjid $subc -i "$rawdir"$subc"/rawdata/anat/"$subc"_T1w.nii.gz" -all
