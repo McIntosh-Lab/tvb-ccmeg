04c29ecc1ca7b14b209a06922c1e3f4fdedf293e
JP-Solomon <jpsolomo@sfu.ca>
2025-01-09 11:47:23 -0800

    add file transfer commands for VV data

--- batch_scripts/submit_transfer_file_job.sh
+++ batch_scripts/submit_transfer_file_job.sh
@@ -17,9 +17,16 @@ fi
 rsync -avzh --no-g --no-p --partial --progress $HOME/projects/def-rmcintos/Cam-CAN/meg $(pwd)/_Data/
 
 # If you want to run freesurfer yourself copy the raw MRI data below
-#rsync -avzh --no-g --no-p --partial --progress $HOME/projects/def-rmcintos/Cam-CAN/mri/raw $(pwd)/_Data/mri/
+# rsync -avzh --no-g --no-p --partial --progress $HOME/projects/def-rmcintos/Cam-CAN/mri/raw $(pwd)/_Data/mri/
 
 # OR
 
 # Copy the freesurfer data to scratch
 rsync -avzh --no-g --no-p --partial --progress $HOME/projects/def-rmcintos/data-sets/Cam-CAN/MEG/freesurfer $(pwd)/_Data/mri/
+
+rsync -avzh --no-g --no-p --partial --progress $HOME/projects/rpp-doesburg/databases/camcan872/cc700/meg/pipeline/release005/BIDSsep/derivatives_rest/aa/AA_nomovecomp/aamod_meg_maxfilt_00001 $(pwd)/_Data/meg/release005/BIDSsep/derivatives_rest/aa/AA_nomovecomp/aamod_meg_maxfilt_00001/
+
+rsync -avzh --no-g --no-p --partial --progress $HOME/projects/rpp-doesburg/databases/camcan872/cc700/meg/pipeline/release004/BIDS_20190411/meg_emptyroom $(pwd)/_Data/meg/release004/BIDS_20190411/
+
+rsync -avzh --no-g --no-p --partial --progress $HOME/projects/rpp-doesburg/databases/camcan_coreg/trans $(pwd)/_Data/meg/camcan_coreg/
+
