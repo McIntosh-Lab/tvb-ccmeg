9f0ffae1b7abf5fd3bf2f3ae2a1a28734ff660b0
JP-Solomon <jack.p.solomon@gmail.com>
2025-08-05 16:18:58 -0700

    fix pipeline implentation of visualization.py

--- tvb-ccmeg/pipeline_rest_beamformer.py
+++ tvb-ccmeg/pipeline_rest_beamformer.py
@@ -11,7 +11,7 @@
 import mne              # Need MNE Python
 import preprocess       # Module with all the preprocessing functions
 import compute_source   # Module with functions to go from sensor space to source space
-import visualization	# Module with plotting functions
+import visualization    # Module with plotting functions
 import numpy as np      # Need for array operations
 import os
 import sys
@@ -75,7 +75,7 @@ plot_head_pos = False
 surfer_kwargs = dict(
 	surface='inflated',       # Surface type for visualization
 	hemi='lh',             # Split the brain into left and right hemispheres
-	subject='fsaverage',      # Subject to use for visualization (fsaverage is a template)
+	subject='fsaverage6',      # Subject to use for visualization (fsaverage is a template)
 	subjects_dir=fs_dir,      # Directory containing the subject's FreeSurfer data
 	views="medial",           # Initial view orientation (e.g., medial)
 	colormap='jet',           # Colormap for data visualization
@@ -237,11 +237,11 @@ np.save(os.path.join(output_dir, 'stc_ts_PSD'), stc_ts_PSD)
 np.save(os.path.join(output_dir, 'source_PSD_freq'), source_PSD_freq)
 
 # Morph to fsAverage
-stc_fsAvg = compute_source.morph_2_fsaverage(stc, fs_dir, subject)
+stc_fsAvg, morph = compute_source.morph_2_fsaverage(stc, fs_dir, subject)
 stc_fsAvg.save(os.path.join(output_dir, 'stc_fsAverage_beamformer'), overwrite=True)
 
 # Add source level PSD topomap to report
-report.add_figure(fig=compute_source.stc_band_power_plot(), title='Source Level Band Power')
+report.add_figure(fig=visualization.stc_band_power_plot(stc, stc_power_bands, morph, subject, surfer_kwargs), title='Source Level Band Power')
 
 # Parcellate_Source_Data
 labels_aparc, labels_schaefer, parc_ts_aparc, parc_ts_schaefer = compute_source.parcellate_source_data(src, stc, subject, fs_dir, output_dir, Vol, mode = 'pca_flip')
